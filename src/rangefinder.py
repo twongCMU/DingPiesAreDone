fee6534b987aeba3b712fa3e79e06eb8f2ef7a3e
TwongCMU <github@polynomial.org>
2020-10-31 11:35:45 -0700

    Increase the minimum timer for the rangerfinder

--- src/rangefinder.py
+++ src/rangefinder.py
@@ -46,8 +46,8 @@ class RangeFinder:
 
                 minutes = int(round((distance_feet - .5) * 4))
 
-		# the lowest value is 1 minute but we already have a gesture for that
-                minutes += 1
+		# a value of 0 isn't useful and we already handle the 1 minute case
+                minutes += 2
 
                 if minutes < 10:
                     rh.display.print_str("000" + str(minutes))
